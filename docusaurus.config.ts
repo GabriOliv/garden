8ca5d7e8f22bcdb14f3defae62f6d81b51b231e5
Gabriel Laureano <gabriel.oliveira@simeon.com.br>
2025-05-25 17:32:29 -0300

    Enable hideable sidebar and auto-collapse categories in documentation

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -87,6 +87,12 @@ const config: Config = {
 			disableSwitch: false,
 			respectPrefersColorScheme: true,
 		},
+		docs: {
+			sidebar: {
+				hideable: true,
+				autoCollapseCategories: true,
+			},
+		},
 		navbar: {
 			title: 'Garden',
 			// logo: {
