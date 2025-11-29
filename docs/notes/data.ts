967797030a58f28b9c53041b95f6705029271e47
Gabriel Laureano <gabriel.oliveira@simeon.com.br>
2025-11-29 16:12:58 -0300

    feat: adicionar arquivo data.ts para gerenciar front matter das notas

---
+++ docs/notes/data.ts
@@ -0,0 +1,11 @@
+// Importing front matter from note files
+import { frontMatter as peltzman_effect } from './peltzman_effect.mdx';
+import { frontMatter as e2e_useful } from './e2e_useful.mdx';
+import { frontMatter as test_coverage_sdlc } from './test_coverage_sdlc.mdx';
+
+// Exporting an object that maps note identifiers to their front matter
+export default {
+  peltzman_effect,
+  e2e_useful,
+  test_coverage_sdlc,
+};
